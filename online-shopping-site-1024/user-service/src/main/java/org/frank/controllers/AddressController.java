f7752fa02c7e12885e663b06e58d12a37caeaedb
fengandzhy <fengandzhy@gmail.com>
2024-04-16 21:23:08 +1200

    online-shopping-site-1024 controllers

---
+++ online-shopping-site-1024/user-service/src/main/java/org/frank/controllers/AddressController.java
@@ -0,0 +1,21 @@
+package org.frank.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 电商-公司收发货地址表 前端控制器
+ * </p>
+ *
+ * @author frank
+ * @since 2024-04-16
+ */
+@RestController
+@RequestMapping("/addressDO")
+public class AddressController {
+
+}
+
---
+++ online-shopping-site-1024/user-service/src/main/java/org/frank/controllers/UserController.java
@@ -0,0 +1,21 @@
+package org.frank.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author frank
+ * @since 2024-04-16
+ */
+@RestController
+@RequestMapping("/userDO")
+public class UserController {
+
+}
+
