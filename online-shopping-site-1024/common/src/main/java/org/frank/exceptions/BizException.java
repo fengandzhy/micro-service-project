4aa6a707b0b98d3335fb3b84ddabc5066a12f38c
fengandzhy <fengandzhy@gmail.com>
2024-04-20 09:19:47 +1200

    online-shopping-site-1024 BizException

---
+++ online-shopping-site-1024/common/src/main/java/org/frank/exceptions/BizException.java
@@ -0,0 +1,23 @@
+package org.frank.exceptions;
+
+import lombok.Data;
+import org.frank.enums.BizCodeEnum;
+
+@Data
+public class BizException extends RuntimeException{
+
+    private Integer code;
+    private String msg;
+
+    public BizException(Integer code, String message) {
+        super(message);
+        this.code = code;
+        this.msg = message;
+    }
+
+    public BizException(BizCodeEnum bizCodeEnum) {
+        super(bizCodeEnum.getMessage());
+        this.code = bizCodeEnum.getCode();
+        this.msg = bizCodeEnum.getMessage();
+    }
+}
