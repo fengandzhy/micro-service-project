886900b2f53fab08f900931020d84c64ca2c75a4
fengandzhy <fengandzhy@gmail.com>
2024-04-20 09:29:38 +1200

    online-shopping-site-1024 CustomerExceptionHandler

--- online-shopping-site-1024/common/src/main/java/org/frank/exceptions/BizException.java
+++ online-shopping-site-1024/common/src/main/java/org/frank/exceptions/BizException.java
@@ -7,17 +7,17 @@ import org.frank.enums.BizCodeEnum;
 public class BizException extends RuntimeException{
 
     private Integer code;
-    private String msg;
+    private String message;
 
     public BizException(Integer code, String message) {
         super(message);
         this.code = code;
-        this.msg = message;
+        this.message = message;
     }
 
     public BizException(BizCodeEnum bizCodeEnum) {
         super(bizCodeEnum.getMessage());
         this.code = bizCodeEnum.getCode();
-        this.msg = bizCodeEnum.getMessage();
+        this.message = bizCodeEnum.getMessage();
     }
 }
